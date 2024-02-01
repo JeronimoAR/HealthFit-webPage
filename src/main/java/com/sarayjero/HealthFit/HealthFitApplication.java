0d029cae6b546a24ebcadf88dfb1844c625cd9b0
Jeronimo Arias <jeronimoarias1212@gmail.com>
2024-01-31 19:23:12 -0500

    First commit

---
+++ src/main/java/com/sarayjero/HealthFit/HealthFitApplication.java
@@ -0,0 +1,13 @@
+package com.sarayjero.HealthFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthFitApplication.class, args);
+	}
+
+}
